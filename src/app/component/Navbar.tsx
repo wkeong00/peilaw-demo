6767c5c9ae4fcbff7baef206cc06135301b01e78
Chris Wei keong Ngu <wngu@Lenovo.com>
2026-08-17 15:40:24 +0800

    update for new design 2 about privacy

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -5,9 +5,9 @@ import React, { useState } from 'react';
 import Link from "next/link";
 import Image from "next/image";
 import { Home, Info, Handshake, Mail } from "lucide-react";
-import { Cormorant_Garamond } from 'next/font/google';
+import { Playfair_Display } from 'next/font/google';
 
-const cormorant = Cormorant_Garamond({ subsets: ['latin'], weight: ['700'] });
+const playfair = Playfair_Display({ subsets: ['latin'], weight: ['700'] });
 
 
 const Navbar = () => {
@@ -27,7 +27,7 @@ const Navbar = () => {
                 <Link href="/" className="flex items-center space-x-2 text-2xl font-bold text-white tracking-wide">
                     <Image src="/caologo.png" alt="Logo" width={82} height={82} />
                     <span
-                        className={`${cormorant.className} text-3xl font-extrabold bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-700 bg-clip-text text-transparent tracking-wide drop-shadow-lg`}
+                        className={`${playfair.className} text-3xl font-extrabold bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-700 bg-clip-text text-transparent tracking-wide drop-shadow-lg`}
                         style={{ letterSpacing: '2px' }}
                     >
                         Cao Legal Solutions
